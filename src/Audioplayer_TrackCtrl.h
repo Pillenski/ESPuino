c4fa16c4f873d33f309e131980b3ba51c290e1da
Sebastian Reiche <s.reiche@lebenshilfe-dresden.de>
2025-05-28 14:16:57 +0200

    correct the non-sense comments in AudioPlayer_TrackCtrl.h

--- src/Audioplayer_TrackCtrl.h
+++ src/Audioplayer_TrackCtrl.h
@@ -8,7 +8,7 @@ namespace AudioPlayer_TrackCtrl {
      * Execute the full track‑control command set that used to live inside the
      * monolithic AudioPlayer_Task() loop.
      *
-     * @param cmd   One of the TRACK_* command constants (see Cmd.h).
+     * @param cmd   One of the Track-Control command constants (see values.h).
      * @param audio Pointer to the global Audio instance. Must be valid.
      */
     void executeTrackCommand(Audio* audio, uint8_t cmd);
