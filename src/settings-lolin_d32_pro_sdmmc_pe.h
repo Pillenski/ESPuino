2ae9c280d8fe06994b93ebc6fd829582efbbf8ba
Pillenski <8606731+Pillenski@users.noreply.github.com>
2025-06-13 22:36:03 +0200

    set Lolin D32 Pro setting according to my hardware

--- src/settings-lolin_d32_pro_sdmmc_pe.h
+++ src/settings-lolin_d32_pro_sdmmc_pe.h
@@ -62,39 +62,39 @@
     #endif
 
     // Amp enable (optional)
-    #define GPIO_PA_EN                      108         // To enable/disable amp for loudspeaker; connected to port-expander
+    //#define GPIO_PA_EN                      99         // To enable/disable amp for loudspeaker; connected to port-expander
 
     // Control-buttons
-    #define NEXT_BUTTON                     102         // Next-Button: connected to port-expander
-    #define PREVIOUS_BUTTON                 100         // Prev-Button: connected to port-expander
-    #define PAUSEPLAY_BUTTON                101         // Pplay-Button: connected to port-expander
-    #define ROTARYENCODER_BUTTON            103         // Set to 99 to disable the button; connected to port-expander
-    #define BUTTON_4                        104         // Button 4: connected to port-expander
-    #define BUTTON_5                        105         // Button 5: connected to port-expander
+    #define NEXT_BUTTON                     103         // Next-Button: connected to port-expander
+    #define PREVIOUS_BUTTON                 101         // Prev-Button: connected to port-expander
+    #define PAUSEPLAY_BUTTON                102         // Pplay-Button: connected to port-expander
+    #define ROTARYENCODER_BUTTON            99         // Set to 99 to disable the button; connected to port-expander
+    #define BUTTON_4                        106         // Button 4: connected to port-expander
+    #define BUTTON_5                        100       // Button 5: connected to port-expander
 
     //#define BUTTONS_LED                   114         // Powers the LEDs of the buttons. Make sure the current consumed by the LEDs can be handled by the used GPIO
 
     // Channels of port-expander can be read cyclic or interrupt-driven. It's strongly recommended to use the interrupt-way!
     // Infos: https://forum.espuino.de/t/einsatz-des-port-expanders-pca9555/306
     #ifdef PORT_EXPANDER_ENABLE
-        #define PE_INTERRUPT_PIN            36          // GPIO that is used to receive interrupts from port-expander + to wake up ESP32
+        #define PE_INTERRUPT_PIN            32          // GPIO that is used to receive interrupts from port-expander + to wake up ESP32
     #endif
 
     // I2C-configuration (necessary for PCA9555)
     #ifdef I2C_2_ENABLE
-        #define ext_IIC_CLK                 4           // i2c-SCL (clock)
-        #define ext_IIC_DATA                13          // i2c-SDA (data)
+        #define ext_IIC_CLK                 22           // i2c-SCL (clock)
+        #define ext_IIC_DATA                5          // i2c-SDA (data)
     #endif
 
     // Wake-up button => this also is the interrupt-pin if port-expander is enabled!
     // Please note: only RTC-GPIOs (0, 4, 12, 13, 14, 15, 25, 26, 27, 32, 33, 34, 35, 36, 39, 99) can be used! Set to 99 to DISABLE.
     // Please note #2: this button can be used as interrupt-pin for port-expander. If so, all pins connected to port-expander can wake up ESPuino.
-    #define WAKEUP_BUTTON                   36          // Defines the button that is used to wake up ESPuino from deepsleep; set to 99 to disable
+    #define WAKEUP_BUTTON                   32          // Defines the button that is used to wake up ESPuino from deepsleep; set to 99 to disable
 
     // Power-control
-    #define POWER                           115         // GPIO used to drive transistor-circuit, that switches off peripheral devices while ESP32-deepsleep
+    #define POWER                           33         // GPIO used to drive transistor-circuit, that switches off peripheral devices while ESP32-deepsleep
     #ifdef POWER
-        #define INVERT_POWER                            // If enabled, use inverted logic for POWER circuit, that means peripherals are turned off by writing HIGH
+        //#define INVERT_POWER                            // If enabled, use inverted logic for POWER circuit, that means peripherals are turned off by writing HIGH
     #endif
 
     // (optional) Neopixel
@@ -110,7 +110,7 @@
     #ifdef MEASURE_BATTERY_VOLTAGE
         #define VOLTAGE_READ_PIN            35          // GPIO used to monitor battery-voltage. Don't change, it's built in
         constexpr float referenceVoltage = 3.30;        // Voltage between 3.3V and GND-pin at the develboard in battery-mode (disconnect USB!)
-        constexpr float offsetVoltage = 0.1;            // If voltage measured by ESP isn't 100% accurate, you can add an correction-value here
+        constexpr float offsetVoltage = 0.15;           // If voltage measured by ESP isn't 100% accurate, you can add an correction-value here
     #endif
 
     // (optional) For measuring battery-voltage a voltage-divider is already onboard. Connect a LiPo and use it!
