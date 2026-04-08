ef29092a4424f8678d805d37c12dbc634d2dd3e4
Pillenski <8606731+Pillenski@users.noreply.github.com>
2026-04-08 22:49:08 +0200

    refactor: clean up clang-formatting-errors

--- src/Bluetooth.cpp
+++ src/Bluetooth.cpp
@@ -181,7 +181,8 @@ void Bluetooth_Init(void) {
 			.bck_io_num = I2S_BCLK,
 			.ws_io_num = I2S_LRC,
 			.data_out_num = I2S_DOUT,
-			.data_in_num = I2S_PIN_NO_CHANGE};
+			.data_in_num = I2S_PIN_NO_CHANGE,
+		};
 		a2dp_sink->set_pin_config(pin_config);
 		a2dp_sink->activate_pin_code(false);
 	#ifdef PLAY_MONO_SPEAKER
